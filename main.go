42798597a30fee13a5d2c0c29559d57c7c8492a2
Sreenivasan K <sreeramu@gmail.com>
2018-06-12 17:51:58 +0530

    Update main.go

--- main.go
+++ main.go
@@ -18,8 +18,6 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
